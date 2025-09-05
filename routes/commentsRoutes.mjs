304876969cd548361e0185195e38189f228c8448
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-05 15:41:22 -0400

    Added patch  to comment/:id route

--- routes/commentsRoutes.mjs
+++ routes/commentsRoutes.mjs
@@ -7,47 +7,63 @@ const router = express.Router();
 
 router
     .route("/")
-        .get((req, res)=>{
-            res.json(comments);
-        })
-        .post((req, res)=>{
-            const { userId, postId, body } = req.body; // grabing these values from the req.body
-            let id;
-            if(comments.length > 0){
-                id = comments[comments.length - 1].id + 1;
-            } else{
-                id = 1;
-            }
-            
+    .get((req, res) => {
+        res.json(comments);
+    })
+    .post((req, res) => {
+        const { userId, postId, body } = req.body; // grabing these values from the req.body
+        let id;
+        if (comments.length > 0) {
+            id = comments[comments.length - 1].id + 1;
+        } else {
+            id = 1;
+        }
 
-            if(userId && postId && body){
-                const comment = {
+
+        if (userId && postId && body) {
+            const comment = {
                 id: id,
                 userId: userId,
                 postId: postId,
                 body: body
-                }
-                comments.push(comment);
-                res.json(comments);
-                } else{
-                    res.status(400).json({msg: "Insuffecient Data"});
-                }
-        })
+            }
+            comments.push(comment);
+            res.json(comments);
+        } else {
+            res.status(400).json({ msg: "Insuffecient Data" });
+        }
+    })
 
 
 router
     .route("/:id")
-        .get((req, res, next)=>{
-            const id = req.params.id;
-            let comment = comments.find((comment) => comment.id == id);
+    .get((req, res, next) => {
+        const id = req.params.id;
+        let comment = comments.find((comment) => comment.id == id);
 
-            if(comment){
-                res.json(comment);
-            } else next();
+        if (comment) {
+            res.json(comment);
+        } else next();
 
+    })
+    .patch((req, res, next) => {
+        const id = req.params.id;
+        const data = req.body;
+        const comment = comments.find((comment, i) => { // find the comment
+            if (comment.id == id) {
+                for (let item in data) {
+                    comments[i][item] = data[item]; // make the change
+                }
+                return true;
+            }
         })
+        // send a response
+        if (comment) {
+            res.json(comments);
+        } else next();
+
+    })
 
 
 
 export default router;
-        
